4bbe2ab3b995ef74af3bbd175193b7b64d98b165
Bill Hassett <BillHassett76@gmail.com>
2019-02-10 21:41:31 -0800

    update script with correct file paths

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-cd Desktop/ssg-branch
-cat templates/top.html docs/about.html templates/bottom.html > index.html
-cat templates/top.html docs/education.html templates/bottom.html > education.html
-cat templates/top.html docs/experience.html templates/bottom.html > experience.html
-cat templates/top.html docs/contact.html templates/bottom.html > contact.html
+cat templates/top.html content/about.html templates/bottom.html > docs/index.html
+cat templates/top.html content/education.html templates/bottom.html > docs/education.html
+cat templates/top.html content/experience.html templates/bottom.html > docs/experience.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
