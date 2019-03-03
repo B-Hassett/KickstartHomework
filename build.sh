921ed807b83c9eca0c2f9af14616daec8960976c
Bill Hassett <BillHassett76@gmail.com>
2019-03-03 14:34:55 -0800

    clean up

--- build.sh
+++
@@ -1,4 +0,0 @@
-cat templates/top.html content/about.html templates/bottom.html > docs/index.html
-cat templates/top.html content/education.html templates/bottom.html > docs/education.html
-cat templates/top.html content/experience.html templates/bottom.html > docs/experience.html
-cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
---
+++ manage.py
@@ -0,0 +1,8 @@
+#IMPORT FUNCTIONS TO BUILD SITE
+
+import utils
+if __name__ == '__main__':
+   utils.main()
+   utils.create_list()
+   utils.build()
+    
